ebfe77020b0964e878d0370b16b5d2d82bf073b9
Pavel Kuznetsov <Technic47@gmail.com>
2026-01-13 19:25:26 +0300

    Init commit.

---
+++ src/test/java/ru/kuznetsov/shop/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuznetsov.shop.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
